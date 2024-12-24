de853cb4b45a8e7db89d4b11f5cbc81b5b0f1173
armagansenol <armagansnl@gmail.com>
2024-12-24 21:21:15 +0300

    Refactor ShopPage component for improved readability and consistency
    - Updated import statements to use single quotes for consistency.
    - Enhanced code structure by formatting JSX elements for better readability.
    - Adjusted spacing and indentation in the component layout.
    - Ensured consistent usage of tags in sanityFetch calls.
    - Improved button theming for better visual presentation.

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,47 +1,59 @@
-import s from "./shop.module.scss"
+import s from './shop.module.scss';
 
-import cn from "clsx"
+import { cn } from '@/lib/utils';
 
-import { AnimatedCard } from "@/components/animated-card"
-import { Link } from "@/components/utility/link"
-import { routes } from "@/lib/constants"
-import { ANIMATED_CARDS_QUERY } from "@/lib/queries/sanity/animatedCards"
-import { sanityFetch } from "@/lib/sanity/client"
-import { AnimatedCardProps } from "types"
-import { Button } from "@/components/ui/button"
+import { AnimatedCard } from '@/components/animated-card';
+import { Button } from '@/components/ui/button';
+import { Link } from '@/components/utility/link';
+import { routes } from '@/lib/constants';
+import { ANIMATED_CARDS_QUERY } from '@/lib/queries/sanity/animatedCards';
+import { sanityFetch } from '@/lib/sanity/client';
+import { AnimatedCardProps } from 'types';
 
 export default async function ShopPage() {
-  const cards = await sanityFetch<AnimatedCardProps[]>({ query: ANIMATED_CARDS_QUERY, tags: ["animatedCards"] })
-  console.log("cards", cards)
+  const cards = await sanityFetch<AnimatedCardProps[]>({
+    query: ANIMATED_CARDS_QUERY,
+    tags: ['animatedCards'],
+  });
+  console.log('cards', cards);
 
   return (
-    <section className={cn(s.shop, "flex flex-col items-center")}>
+    <section className={cn(s.shop, 'flex flex-col items-center')}>
       <h2>Impossible to Choose Just One!</h2>
       <p>Can’t decide? Try them all and discover your new favorite!</p>
       <div className="flex flex-col items-center tablet:grid grid-cols-4 gap-16 mt-10 tablet:mt-20 px-4 tablet:px-0">
         {cards.map((item) => {
           return (
             <div
-              className={cn(s.card, "flex flex-col items-center tablet:items-stretch gap-5 tablet:gap-10")}
+              className={cn(
+                s.card,
+                'flex flex-col items-center tablet:items-stretch gap-5 tablet:gap-10',
+              )}
               key={item.id}
             >
-              <Link href={`/${routes.shop.url}/${item.product.shopifySlug}`} prefetch={true}>
+              <Link
+                href={`/${routes.shop.url}/${item.product.shopifySlug}`}
+                prefetch={true}
+              >
                 <AnimatedCard {...item} />
               </Link>
               <div className="flex flex-row tablet:flex-col items-stretch gap-2">
-                <Button asChild variant="highlighted" size="sm" padding="slim">
-                  <Link href={`/${routes.shop.url}/${item.product.shopifySlug}`} prefetch={true}>
+                <Button colorTheme="blueRuin" size="sm" padding="slim" asChild>
+                  <Link
+                    href={`/${routes.shop.url}/${item.product.shopifySlug}`}
+                    prefetch={true}
+                  >
                     SHOP NOW
                   </Link>
                 </Button>
-                <Button variant="default" size="sm" padding="slim">
+                <Button colorTheme="invertedBlueRuin" size="sm" padding="slim">
                   ADD TO CART
                 </Button>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </section>
-  )
+  );
 }
