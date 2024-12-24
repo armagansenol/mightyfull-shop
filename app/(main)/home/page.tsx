355b4420fbdf2ce59b4aa845b74295fd8a28b321
armagansenol <armagansnl@gmail.com>
2024-12-24 21:20:57 +0300

    Refactor HomePage component for improved readability and consistency
    - Updated import statements to use single quotes for consistency.
    - Enhanced code structure by formatting JSX elements for better readability.
    - Adjusted spacing and indentation in the component layout.
    - Ensured consistent usage of tags in sanityFetch calls.
    - Improved overall styling and layout for better visual presentation.

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,51 +1,72 @@
-import s from "./home.module.scss"
+import s from './home.module.scss';
 
-import cn from "clsx"
+import cn from 'clsx';
 
-import { AnimatedCard } from "@/components/animated-card"
-import { FadeInOutCarousel } from "@/components/fade-in-out-carousel"
-import { FeatureHighlight } from "@/components/feature-highlight"
-import { IconCloud2, IconLeftArm, IconRightArm } from "@/components/icons"
-import { Marquee } from "@/components/marquee"
-import { PackageAnimation } from "@/components/package-animation"
-import { Parallax } from "@/components/parallax"
-import { ProductHighlightCarousel } from "@/components/product-highlight-carousel"
-import { Button } from "@/components/ui/button"
-import { Img } from "@/components/utility/img"
-import { Link } from "@/components/utility/link"
-import { routes } from "@/lib/constants"
-import { ANIMATED_CARDS_QUERY } from "@/lib/queries/sanity/animatedCards"
-import { FEATURE_HIGHLIGHT_QUERY } from "@/lib/queries/sanity/featureHighlightQuery"
-import { PRODUCT_HIGHLIGHT_QUERY } from "@/lib/queries/sanity/productHighlight"
-import { TESTIMONIALS_QUERY } from "@/lib/queries/sanity/testimonials"
-import { sanityFetch } from "@/lib/sanity/client"
-import { AnimatedCardProps, FeatureHighlightQueryResult, ProductHighlightQueryResult, Testimonial } from "@/types"
+import { AnimatedCard } from '@/components/animated-card';
+import { FadeInOutCarousel } from '@/components/fade-in-out-carousel';
+import { FeatureHighlight } from '@/components/feature-highlight';
+import { IconCloud2, IconLeftArm, IconRightArm } from '@/components/icons';
+import { Marquee } from '@/components/marquee';
+import { PackageAnimation } from '@/components/package-animation';
+import { Parallax } from '@/components/parallax';
+import { ProductHighlightCarousel } from '@/components/product-highlight-carousel';
+import { Button } from '@/components/ui/button';
+import { Img } from '@/components/utility/img';
+import { Link } from '@/components/utility/link';
+import { routes } from '@/lib/constants';
+import { ANIMATED_CARDS_QUERY } from '@/lib/queries/sanity/animatedCards';
+import { FEATURE_HIGHLIGHT_QUERY } from '@/lib/queries/sanity/featureHighlightQuery';
+import { PRODUCT_HIGHLIGHT_QUERY } from '@/lib/queries/sanity/productHighlight';
+import { TESTIMONIALS_QUERY } from '@/lib/queries/sanity/testimonials';
+import { sanityFetch } from '@/lib/sanity/client';
+import {
+  AnimatedCardProps,
+  FeatureHighlightQueryResult,
+  ProductHighlightQueryResult,
+  Testimonial,
+} from '@/types';
 
 export default async function HomePage() {
   const { productHighlight } = await sanityFetch<ProductHighlightQueryResult>({
     query: PRODUCT_HIGHLIGHT_QUERY,
-    tags: ["productHighlight"],
-  })
+    tags: ['productHighlight'],
+  });
   const { featureHighlight } = await sanityFetch<FeatureHighlightQueryResult>({
     query: FEATURE_HIGHLIGHT_QUERY,
-    tags: ["featureHighlight"],
-  })
-  const testimonials = await sanityFetch<Testimonial[]>({ query: TESTIMONIALS_QUERY, tags: ["testmonials"] })
-  const cards = await sanityFetch<AnimatedCardProps[]>({ query: ANIMATED_CARDS_QUERY, tags: ["animatedCards"] })
+    tags: ['featureHighlight'],
+  });
+  const testimonials = await sanityFetch<Testimonial[]>({
+    query: TESTIMONIALS_QUERY,
+    tags: ['testmonials'],
+  });
+  const cards = await sanityFetch<AnimatedCardProps[]>({
+    query: ANIMATED_CARDS_QUERY,
+    tags: ['animatedCards'],
+  });
 
   return (
     <>
-      <section className={cn(s.intro, "flex flex-col items-stretch tablet:grid grid-cols-12")}>
-        <div className={cn(s.text, "col-span-6 flex flex-col items-center tablet:items-start justify-center")}>
+      <section
+        className={cn(
+          s.intro,
+          'flex flex-col items-stretch tablet:grid grid-cols-12',
+        )}
+      >
+        <div
+          className={cn(
+            s.text,
+            'col-span-6 flex flex-col items-center tablet:items-start justify-center',
+          )}
+        >
           <h1>
-            This{" "}
+            This{' '}
             <span className={s.might}>
               <span>might</span>
-            </span>{" "}
-            be the{" "}
+            </span>{' '}
+            be the{' '}
             <span className={s.best}>
               <span>best cookie</span>
-            </span>{" "}
+            </span>{' '}
             ever!
           </h1>
           <p>Meet our mightyfull flavors.</p>
@@ -76,36 +97,65 @@ export default async function HomePage() {
           <div className="flex items-center">
             <div className={s.marqueeItem}>PROTEIN PACKED COOKIES!</div>
             <div className={s.imgC}>
-              <Img src="/img/c-pb-jelly-choco-chip.png" height={100} width={100} alt="Cookie" />
+              <Img
+                src="/img/c-pb-jelly-choco-chip.png"
+                height={100}
+                width={100}
+                alt="Cookie"
+              />
             </div>
-            <div className={s.marqueeItem}>FEEL THE MIGHT, SAVOR THE FULLNESS!</div>
+            <div className={s.marqueeItem}>
+              FEEL THE MIGHT, SAVOR THE FULLNESS!
+            </div>
             <div className={s.imgC}>
-              <Img src="/img/c-double-choco-chip.png" height={100} width={100} alt="Cookie" />
+              <Img
+                src="/img/c-double-choco-chip.png"
+                height={100}
+                width={100}
+                alt="Cookie"
+              />
             </div>
           </div>
         </Marquee>
       </section>
       {productHighlight.items.length > 0 && (
-        <section className={cn(s.highlights, "py-10 tablet:py-20")}>
-          <section className={cn(s.shop, "flex flex-col items-center")}>
+        <section className={cn(s.highlights, 'py-10 tablet:py-20')}>
+          <section className={cn(s.shop, 'flex flex-col items-center')}>
             <h2>Impossible to Choose Just One!</h2>
             <p>Can’t decide? Try them all and discover your new favorite!</p>
             {/* MOBILE */}
             <div className="block tablet:hidden">
-              <ProductHighlightCarousel items={cards} options={{ loop: true }} />
+              <ProductHighlightCarousel
+                items={cards}
+                options={{ loop: true }}
+              />
             </div>
             {/* DESKTOP */}
             <div className="hidden tablet:block">
               <div className="grid grid-cols-4 gap-12 mt-20 flex-shrink-0">
                 {cards.map((item) => {
                   return (
-                    <div className={cn(s.card, "flex flex-col gap-10")} key={item.id}>
-                      <Link href={`/${routes.shop.url}/${item.product.shopifySlug}`} prefetch={true}>
+                    <div
+                      className={cn(s.card, 'flex flex-col gap-10')}
+                      key={item.id}
+                    >
+                      <Link
+                        href={`/${routes.shop.url}/${item.product.shopifySlug}`}
+                        prefetch={true}
+                      >
                         <AnimatedCard {...item} />
                       </Link>
                       <div className="flex flex-col items-stretch space-y-2">
-                        <Button asChild variant="highlighted" size="sm" padding="slim">
-                          <Link href={`/${routes.shop.url}/${item.product.shopifySlug}`} prefetch={true}>
+                        <Button
+                          colorTheme="blueRuin"
+                          size="sm"
+                          padding="slim"
+                          asChild
+                        >
+                          <Link
+                            href={`/${routes.shop.url}/${item.product.shopifySlug}`}
+                            prefetch={true}
+                          >
                             SHOP NOW
                           </Link>
                         </Button>
@@ -114,7 +164,7 @@ export default async function HomePage() {
                         </Button>
                       </div>
                     </div>
-                  )
+                  );
                 })}
               </div>
             </div>
@@ -129,10 +179,13 @@ export default async function HomePage() {
       <div className="relative bg-[var(--blue-ruin)] p-2 tablet:p-5 overflow-hidden">
         <div className="absolute bottom-0 left-0 right-0 h-2 tablet:h-5 bg-[var(--blue-ruin)] z-50"></div>
         <section
-          className={cn(s.theStory, "flex flex-col items-stretch p-5 pt-24 tablet:pt-60 bg-[var(--sugar-milk)]")}
+          className={cn(
+            s.theStory,
+            'flex flex-col items-stretch p-5 pt-24 tablet:pt-60 bg-[var(--sugar-milk)]',
+          )}
         >
-          <div className={cn("flex flex-col items-center flex-1")}>
-            <div className={cn(s.titleC, "flex items-center gap-3")}>
+          <div className={cn('flex flex-col items-center flex-1')}>
+            <div className={cn(s.titleC, 'flex items-center gap-3')}>
               <div className={s.iconC}>
                 <IconLeftArm />
               </div>
@@ -157,8 +210,9 @@ export default async function HomePage() {
               </div>
             </div>
             <p className={cn(s.p, s.desc)}>
-              At Mightyfull, we believe snacking should do more than just satisfy cravings—it should fuel your body,
-              uplift your day, and taste incredible.
+              At Mightyfull, we believe snacking should do more than just
+              satisfy cravings—it should fuel your body, uplift your day, and
+              taste incredible.
               <br />
               <br />
               Our journey began with a simple moment in the kitchen.
@@ -173,7 +227,7 @@ export default async function HomePage() {
               <Img
                 alt="Cookie Crumb"
                 className="object-contain -rotate-12"
-                src={"/img/c3.png"}
+                src={'/img/c3.png'}
                 height={200}
                 width={200}
               />
@@ -181,10 +235,21 @@ export default async function HomePage() {
           </div>
         </section>
       </div>
-      <section className={cn(s.testimonials, "flex flex-col-reverse tablet:grid grid-cols-12")}>
+      <section
+        className={cn(
+          s.testimonials,
+          'flex flex-col-reverse tablet:grid grid-cols-12',
+        )}
+      >
         <div className="col-span-5">
           <div className={s.imgC}>
-            <Img className="object-cover" src="/img/lady.jpg" alt="Lady Eating a Cookie" height={1000} width={1000} />
+            <Img
+              className="object-cover"
+              src="/img/lady.jpg"
+              alt="Lady Eating a Cookie"
+              height={1000}
+              width={1000}
+            />
           </div>
         </div>
         <div className="col-span-7 py-20 tablet:py-0 flex items-center justify-center">
@@ -192,16 +257,22 @@ export default async function HomePage() {
             <FadeInOutCarousel options={{ loop: true }}>
               {testimonials.map((item) => {
                 return (
-                  <div className={cn(s.item, "flex flex-col items-center justify-center")} key={item._id}>
+                  <div
+                    className={cn(
+                      s.item,
+                      'flex flex-col items-center justify-center',
+                    )}
+                    key={item._id}
+                  >
                     <div className={s.title}>{item.description}</div>
                     <div className={s.description}>{item.title}</div>
                   </div>
-                )
+                );
               })}
             </FadeInOutCarousel>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
