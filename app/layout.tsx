91e9bb099619364f7071ed500ce39b636df7b422
armagansenol <armagansnl@gmail.com>
2024-12-26 14:44:18 +0300

    Refactor configuration and layout files for improved consistency and readability
    - Updated import statements to use single quotes for consistency across files.
    - Enhanced the `next.config.mjs` file by adding a `rewrites` section and improving the formatting of existing code.
    - Removed the `page.tsx` file from the main directory as it is no longer needed.
    - Reformatted JSX elements in `layout.tsx` for better readability and structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
-import "styles/global.scss"
-import "styles/tailwind-initial.css"
+import 'styles/global.scss';
+import 'styles/tailwind-initial.css';
 
-import { Providers } from "@/components/providers"
+import { Providers } from '@/components/providers';
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      {/* <head>
+        <meta name="oke:subscriber_id" content={process.env.OKENDO_USER_ID} />
+        <script
+          async
+          src="https://cdn-static.okendo.io/reviews-widget-plus/js/okendo-reviews.js"
+        ></script>
+      </head> */}
       <body className={`antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,46 @@
 /** @type {import('next').NextConfig} */
-import path from "path"
-import { fileURLToPath } from "url"
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
-const __dirname = path.dirname(__filename) // get the name of the directory
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
       },
     ],
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import 'styles/_functions';`,
   },
-  redirects: async () => {
-    return [
-      {
-        source: "/home",
-        destination: "/",
-        permanent: true,
-      },
-    ]
-  },
-}
+  redirects: async () => [
+    {
+      source: '/home',
+      destination: '/',
+      permanent: true,
+    },
+  ],
+  rewrites: async () => [
+    {
+      source: '/',
+      destination: '/home',
+    },
+  ],
+};
 
-export default nextConfig
+export default nextConfig;
