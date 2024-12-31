c10c5558aa0849907c8406daeaeeacf9a5f21ade
armagansenol <armagansnl@gmail.com>
2024-12-31 13:42:18 +0300

    refactor: update button component and carousel styles for improved responsiveness
    - Modified styles in the product highlight carousel to enhance button positioning and mobile responsiveness, increasing right and left button margins from 16px to 40px and changing button positions from 50% to 30%.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,30 +1,30 @@
-import s from "@/styles/buttons.module.scss"
+import s from '@/styles/buttons.module.scss';
 
-import { cn } from "@/lib/utils"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
-import * as React from "react"
+import { cn } from '@/lib/utils';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-xl whitespace-nowrap focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 tablet:[&_svg]:size-6 [&_svg]:shrink-0 [&>*]:pointer-events-none",
+  'inline-flex items-center justify-center rounded-xl whitespace-nowrap focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 tablet:[&_svg]:size-6 [&_svg]:shrink-0 [&>*]:pointer-events-none',
   {
     variants: {
       variant: {
-        link: "underline-offset-4 hover:underline",
+        link: 'underline-offset-4 hover:underline',
         default: s.default,
-        highlighted: s.highlighted,
+        highlighted: s.highlighted
       },
       size: {
         sm: s.sm,
         md: s.md,
         lg: s.lg,
-        icon: "h-9 w-9",
+        icon: 'h-9 w-9'
       },
       padding: {
-        fat: "py-4 px-6 tablet:py-5 tablet:px-12 rounded-lg",
-        slim: "py-2 px-5 tablet:py-3 tablet:px-none w-full rounded-md tablet:rounded-lg",
-        none: "w-full h-full rounded-lg",
-        square: "p-4",
+        fat: 'py-4 px-6 tablet:py-5 tablet:px-12 rounded-lg',
+        slim: 'py-4 px-5 tablet:py-3 tablet:px-none w-full rounded-md tablet:rounded-lg',
+        none: 'w-full h-full rounded-lg',
+        square: 'p-4'
       },
       colorTheme: {
         blueRuin: s.blueRuin,
@@ -33,46 +33,54 @@ const buttonVariants = cva(
         invertedThemed: s.invertedThemed,
         nakedBlueRuin: s.nakedBlueRuin,
         nakedThemed: s.nakedThemed,
-        nakedFull: s.nakedFull,
-      },
+        nakedFull: s.nakedFull
+      }
     },
     defaultVariants: {
-      variant: "default",
-      size: "md",
-    },
+      variant: 'default',
+      size: 'md'
+    }
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { className, variant = "default", size = "md", padding = "fat", colorTheme = "themed", asChild = false, ...props },
+    {
+      className,
+      variant = 'default',
+      size = 'md',
+      padding = 'fat',
+      colorTheme = 'themed',
+      asChild = false,
+      ...props
+    },
     ref
   ) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(
-          "cursor-pointer",
+          'cursor-pointer',
           buttonVariants({
             variant,
             size,
             padding,
             colorTheme,
-            className,
+            className
           })
         )}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = 'Button';
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
