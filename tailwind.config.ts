4b52725cd24fbe5170013a876e25304ec1e50ad8
armagansenol <armagansnl@gmail.com>
2025-04-07 17:10:24 +0300

    refactor: simplify Tailwind container configuration
    - Removed redundant padding settings from the Tailwind container configuration for a cleaner setup while maintaining centering functionality.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,14 +12,7 @@ const config: Config = {
   ],
   theme: {
     container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem'
-      }
+      center: true
     },
     extend: {
       gridTemplateColumns: {
